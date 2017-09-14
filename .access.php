4bf06118941087584017e8db047232a7ecb1a6e7
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-14 12:40:43 +0300

    ex1-6

--- .access.php
+++ .access.php
@@ -4,7 +4,6 @@ $PERM["products"]["5"]="W";
 $PERM["services"]["5"]="W";
 $PERM["news"]["5"]="W";
 $PERM["contacts"]["5"]="W";
-$PERM["company"]["5"]="W";
 $PERM["index.php"]["5"]="W";
 $PERM["/"]["*"]="R";
 ?>
