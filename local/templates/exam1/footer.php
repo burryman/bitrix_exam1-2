83a8adc71040cfef5ef2e2cf5635f82a62458663
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-14 16:09:32 +0300

    ex1-15

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -5,24 +5,24 @@
                 <div class="side">
                     <!-- side menu -->
                     <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"left", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "3",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "left",
-		"USE_EXT" => "Y"
-	),
-	false
-);?>
+                    	"bitrix:menu", 
+                    	"left", 
+                    	array(
+                    		"ALLOW_MULTI_SELECT" => "N",
+                    		"CHILD_MENU_TYPE" => "left",
+                    		"COMPONENT_TEMPLATE" => "left",
+                    		"DELAY" => "N",
+                    		"MAX_LEVEL" => "3",
+                    		"MENU_CACHE_GET_VARS" => array(
+                    		),
+                    		"MENU_CACHE_TIME" => "3600",
+                    		"MENU_CACHE_TYPE" => "N",
+                    		"MENU_CACHE_USE_GROUPS" => "Y",
+                    		"ROOT_MENU_TYPE" => "left",
+                    		"USE_EXT" => "Y"
+                    	),
+                    	false
+                    );?>
                     <!-- /side menu -->
                     <!-- side anonse -->
                     <div class="side-block side-anonse">
@@ -52,40 +52,72 @@
                         </div>
                         
                        <!-- footer rew slider box -->
-                       <div class="item-wrap">
-		                     <div class="rew-footer-carousel">
-								<div class="item">
-									<div class="side-block side-opin">
-										<div class="inner-block">
-											<div class="title">
-												<div class="photo-block">
-													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
-												</div>
-												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-												<div class="pos-block">Генеральный директор,"Офис+"</div>
-											</div>
-											<div class="text-block">“В магзине предоставили потрясающий выбор
-												расцветок, а также, получил большую скидку по карте постоянного...</div>
-										</div>
-									</div>
-								</div>
-								<div class="item">
-									<div class="side-block side-opin">
-										<div class="inner-block">
-											<div class="title">
-												<div class="photo-block">
-													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
-												</div>
-												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-												<div class="pos-block">Генеральный директор,"Офис+"</div>
-											</div>
-											<div class="text-block">“В магазине предоставили потрясающий выбор
-												расцветок, а также, получил большую скидку по карте постоянного...</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+                       <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.list", 
+                            "rev_slider", 
+                            array(
+                                "COMPONENT_TEMPLATE" => "rev_slider",
+                                "IBLOCK_TYPE" => "reviews",
+                                "IBLOCK_ID" => "5",
+                                "NEWS_COUNT" => "2",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_BY2" => "NAME",
+                                "SORT_ORDER2" => "ASC",
+                                "FILTER_NAME" => "",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "POSITION",
+                                    1 => "COMPANY",
+                                    2 => "",
+                                ),
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_FILTER" => "N",
+                                "CACHE_GROUPS" => "Y",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "SET_TITLE" => "Y",
+                                "SET_BROWSER_TITLE" => "Y",
+                                "SET_META_KEYWORDS" => "Y",
+                                "SET_META_DESCRIPTION" => "Y",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                                "ADD_SECTIONS_CHAIN" => "Y",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "PAGER_TEMPLATE" => ".default",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "PAGER_TITLE" => "Новости",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => ""
+                            ),
+                            false
+                        );?>
 						<!-- / footer rew slider box --> 
                         
                     </div>
