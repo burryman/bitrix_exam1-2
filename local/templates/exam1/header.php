0738ebe0fc4af46488d3989f684ef5d25f2ac883
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-14 13:00:33 +0300

    ex1-7

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -96,13 +96,15 @@ $workTime = (date('h') > 9) && (date('h') < 18);
         <!-- /nav -->
         <!-- breadcrumbs -->
         <?if($APPLICATION->GetCurPage() != "/"):?>
-            <div class="breadcrumbs-box">
-                <div class="inner-wrap">
-                    <a href="">Главная</a>
-                    <a href="">Мебель</a>
-                    <span>Выставки и события</span>
-                </div>
-            </div>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:breadcrumb",
+                "nav",
+                Array(
+                    "PATH" => "",
+                    "SITE_ID" => "s1",
+                    "START_FROM" => "0"
+                )
+            );?>
         <?endif?>
         <!-- /breadcrumbs -->
         <!-- page -->
