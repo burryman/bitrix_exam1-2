4c2162435bce23b0e1b3b140e30c9f02c961b79e
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-14 14:39:35 +0300

    ex1-14

--- local/templates/exam1/components/bitrix/breadcrumb/nav/template.php
+++ local/templates/exam1/components/bitrix/breadcrumb/nav/template.php
@@ -13,15 +13,8 @@ if(empty($arResult))
 
 $strReturn = '';
 
-//we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
-$css = $APPLICATION->GetCSSArray();
-if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
-{
-	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
-}
 
 $strReturn .= '<div class="breadcrumbs-box"><div class="inner-wrap">';
-
 $itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
