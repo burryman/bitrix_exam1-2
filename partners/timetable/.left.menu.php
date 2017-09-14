0c34aa43ef7517a3174c393b33c9f1b1569b73a2
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-14 10:53:30 +0300

    ex1-1

---
+++ partners/timetable/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partners/timetable/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partners/timetable/archive.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
